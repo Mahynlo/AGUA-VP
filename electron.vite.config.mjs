daab3fd4eeed5d1e1eac6beffcc25a17ec563841
Mahynlo <malcom_navarro12@hotmail.com>
2026-09-12 16:26:13 -0700

    feat: update dependencies and integrate Tailwind CSS with Heroui styles

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 export default defineConfig({
   main: {
@@ -12,9 +13,19 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@heroui/react': resolve('src/renderer/src/components/ui/heroui-bridge.jsx')
       }
     },
-    plugins: [react()]
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss()
+        ]
+      }
+    },
+    plugins: [
+      react()
+    ]
   }
 })
